bbfac8a3bad14069b63ff4b53549a66975a4d7e5
kooksee <kooksee@163.com>
2019-06-20 17:45:25 +0800

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/pubgo/gotask
 
 go 1.12
 
-require github.com/pubgo/assert v0.2.8
+require github.com/pubgo/assert v0.2.9
--- task_test.go
+++ task_test.go
@@ -1,6 +1,7 @@
 package gotask_test
 
 import (
+	"errors"
 	"fmt"
 	"github.com/pubgo/assert"
 	"github.com/pubgo/gotask"
@@ -46,4 +47,43 @@ func TestErrLog(t *testing.T) {
 		}
 	}))
 	task.Wait()
+}
+
+func parserArticleWithReadability(i int) error {
+	errChan := make(chan bool)
+	go func() {
+		time.Sleep(time.Second*4)
+		errChan <- true
+	}()
+
+	for {
+		select {
+		case <-time.After(3 * time.Second):
+			return assert.Wrap(errors.New("readbility timeout"), "等待 %d",i)
+		case <-errChan:
+			return nil
+		}
+	}
+
+}
+
+func TestW(t *testing.T) {
+	var _fn = gotask.TaskOf(func(i int) {
+		assert.ErrWrap(parserArticleWithReadability(i),"yyyy")
+		fmt.Println("ok", i)
+	}, func(err error) {
+		assert.ErrHandle(err, func(err *assert.KErr) {
+			err.P()
+		})
+	})
+
+	var sss = gotask.NewTask(10000, time.Second*2)
+	for i := 0; i < 1000000; i++ {
+		if err := sss.Do(_fn, i); err != nil {
+			panic(err)
+		}
+		fmt.Println(i)
+		fmt.Println(sss.Len())
+	}
+	sss.Wait()
 }
